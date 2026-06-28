a34efc89e3bb24a14b36abdfae3e485fc7900de2
AndreSagrav <127614545+AndreSagrav@users.noreply.github.com>
2026-06-27 20:02:58 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,24 +1,38 @@
-const CACHE_NAME = 'torneo-pro-stats-v1';
+const CACHE_NAME = 'torneo-pro-stats-v2';
 const urlsToCache = [
+    './',
     './index.html',
-    './manifest.json'
+    './manifest.json',
+    './icon.png'
 ];
 
 self.addEventListener('install', event => {
+    self.skipWaiting();
     event.waitUntil(
         caches.open(CACHE_NAME)
             .then(cache => cache.addAll(urlsToCache))
     );
 });
 
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.filter(name => name !== CACHE_NAME)
+                    .map(name => caches.delete(name))
+            );
+        }).then(() => self.clients.claim())
+    );
+});
+
 self.addEventListener('fetch', event => {
     event.respondWith(
-        caches.match(event.request)
+        fetch(event.request)
             .then(response => {
-                if (response) {
-                    return response;
-                }
-                return fetch(event.request);
+                const clone = response.clone();
+                caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
+                return response;
             })
+            .catch(() => caches.match(event.request))
     );
 });
